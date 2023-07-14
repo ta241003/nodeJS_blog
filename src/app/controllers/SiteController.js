4463f0f219762f8bfa9d7f4b61d6858027e63d15
ta241003 <ngotheanh241003@gmail.com>
2023-07-14 22:49:14 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,11 +1,16 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
   // [GET] /
-  index(req, res) {
-    Course.find() 
-      .then(courses => res.json(courses)) 
-      .catch(err => res.status(400).json({ error: 'ERROR' }));
+  index(req, res, next) {
+    Course.find({})
+        .then((courses) => {
+            res.render('home', {
+                courses: mutipleMongooseToObject(courses),
+            });
+        })
+        .catch(next);
   }
 
   // [GET] /search
