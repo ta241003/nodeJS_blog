1bbd310fe74207e81c8e43c7990814d913f15d49
ta241003 <ngotheanh241003@gmail.com>
2023-07-21 17:10:25 +0700

    Sort query helper

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -4,7 +4,7 @@ const mongooseDelete = require('mongoose-delete');
 
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
+const CourseSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, maxLength: 600 },
     image: { type: String },
@@ -15,11 +15,22 @@ const Course = new Schema({
     timestamps: true,
 });
 
+// Custom querry helpers
+CourseSchema.query.sortable = function (req) {   
+    if (req.query.hasOwnProperty('_sort')) {
+        const isValidType = ['asc', 'desc'].includes(req.query.type);
+        return this.sort({
+            [req.query.column]: isValidType ? req.query.type : 'desc',
+        });
+    }
+    return this;
+}
+
 // Add plugins
 mongoose.plugin(slug);
-Course.plugin(mongooseDelete, {
+CourseSchema.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
 });
 
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model('Course', CourseSchema);
